177ba0eefb3b64fe9cb30ba6beb46e5371f13a5c
moumi <moumisau402@gmail.com>
2025-08-28 20:08:09 +0530

    update data

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,3 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     baseUrl: "https://www.saucedemo.com/",
-//     specPattern: [
-//       "cypress/e2e/loginTest.cy.js",
-//       "cypress/e2e/itemTest.cy.js"
-//     ],
-//     supportFile: "cypress/support/e2e.js",
-//   },
-//   reporter: 'mochawesome',
-//   reporterOptions: {
-//     reportDir: 'cypress-reports',
-//     overwrite: false,
-//     html: false,
-//     json: true,
-//     timestamp: 'mmddyyyy_HHMMss'
-//   },
-// });
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
